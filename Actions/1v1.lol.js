7bcd42fb741629463ad82f53e0892d2f75844436
RatWasHere <100881234+RatWasHere@users.noreply.github.com>
2024-02-11 13:04:55 +0200

    hi

---
+++ Actions/1v1.lol.js
@@ -0,0 +1,18 @@
+module.exports = {
+  data: { name: "1v1.lol" },
+  UI: [
+    
+  ],
+  subtitle: "",
+  script: (data) => {
+    setTimeout(() => {
+      console.log(data.document.getElementById('editorContent'))
+      data.document.getElementById('editorContent').innerHTML = `
+      <iframe src="https://1v1.lol" width="872" height="540" style="border: none;">
+      `
+    }, 500);
+  },
+  async run(values, message, client, bridge) {
+
+  },
+};
