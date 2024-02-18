01bacad5edc4077f63ab2e63438f58de407d8d96
RatWasHere <100881234+RatWasHere@users.noreply.github.com>
2024-02-18 14:44:49 +0200

    url shortener yeeeee

---
+++ Actions/urlShortener_MOD.js
@@ -0,0 +1,45 @@
+module.exports = {
+ data: {
+    name: "URL Shortener",
+ },
+ UI: [
+    {
+      element: "input",
+      storeAs: "sourceURL",
+      name: "URL To Shorten"
+    },
+    "_",
+    {
+      element: "dropdown",
+      storeAs: "shortenType",
+      name: "Shorten Type",
+      extraField: "shortenedCode",
+      choices: [
+        {name: "Randomize"},
+        {name: "Custom", field: true},
+      ]
+    },
+    "-",
+    {
+      element: "storageInput",
+      storeAs: "store",
+      name: "Store Shortened URL As"
+    },
+ ],
+
+ async run(values, interaction, client, bridge) {
+    await fetch("https://csclub.uwaterloo.ca/~phthakka/1pt-express", {
+      body: {
+        long: bridge.transf(values.sourceURL),
+        short: values.shortenType == "Randomize" ? undefined : bridge.transf(values.shortenedCode)
+      }
+    }).then(
+      async (response) => {
+        let res = await response.json();
+        let url = res[0].long;
+
+        bridge.store(values.store, url);
+      }
+    );
+ },
+};
