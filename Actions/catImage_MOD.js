d84217dea6505b46ebe2f7d508c879b7e49df448
RatWasHere <100881234+RatWasHere@users.noreply.github.com>
2024-02-18 14:23:56 +0200

    Cat Image by Regianus (stolen from discord) & Create Self Contained Anchor

---
+++ Actions/catImage_MOD.js
@@ -0,0 +1,27 @@
+/*
+ Random cat mod by Regianus (TheKing1543/King)
+ Based off of qshnitzel's "Random Meme" mod
+*/
+module.exports = {
+ data: {
+    name: "Random Cat Image URL",
+ },
+ UI: [
+    "-",
+    {
+      element: "storageInput",
+      storeAs: "store",
+    },
+ ],
+
+ async run(values, interaction, client, bridge) {
+    await fetch("https://api.thecatapi.com/v1/images/search").then(
+      async (response) => {
+        let res = await response.json();
+        let preview = res[0].url;
+
+        bridge.store(values.store, preview);
+      }
+    );
+ },
+};
---
+++ Actions/createSelfContainedAnchor_MOD.js
@@ -0,0 +1,21 @@
+module.exports = {
+  data: { name: "Create Self-Contained Anchor", id: new Date().getTime() },
+  UI: [
+    {
+      element: "input",
+      storeAs: "id",
+      name: "Anchor ID"
+    },
+    "-",
+    {
+      element: "actions",
+      storeAs: "actions",
+      name: "Actions"
+    }
+  ],
+  init: (data, bridge) => {
+    bridge.createGlobal({class: "anchors", name: data.id, value: data.actions});
+  },
+  subtitle: "ID: $[id]$",
+  async run(values, message, client, bridge) {},
+};
