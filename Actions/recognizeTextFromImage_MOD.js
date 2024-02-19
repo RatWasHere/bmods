53ce426dd5f120882b0f60f2552744119d7ac26e
candied apple <50497574+candied-apple@users.noreply.github.com>
2024-02-19 16:19:39 +0300

    Create recognizeTextFromImage_MOD.js
    Requirement
    tesseract.js

---
+++ Actions/recognizeTextFromImage_MOD.js
@@ -0,0 +1,41 @@
+/*
+  Recognize Text From Image Mod by candiedapple
+  Licensed under MIT License
+
+  For support ping me on BMD Discord server.
+*/
+
+module.exports = {
+    data: {
+        name: "Recognize Text From Image",
+    },
+    UI: [
+        {
+            element: "image",
+            storeAs: "image",
+            name: "Image",
+        },
+        "-",
+        {
+            element: "storageInput",
+            storeAs: "store",
+            name: "Store recognized text As",
+        },
+    ],
+
+    async run(values, interaction, client, bridge) {
+
+
+        const { createWorker } = require('tesseract.js');
+
+       await (async () => {
+            const worker = await createWorker();
+
+            const ret = await worker.recognize(await bridge.getImage(values.image));
+            bridge.store(values.store, ret.data.text);
+            await worker.terminate();
+        })();
+
+    },
+};
+
