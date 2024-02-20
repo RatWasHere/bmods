92edf8b1e168ef7c2cf86c2de73bad3220a76645
RatWasHere <100881234+RatWasHere@users.noreply.github.com>
2024-02-20 18:08:50 +0200

    mod dependencies, will work by release 20

---
+++ Actions/dependencies_MOD.js
@@ -0,0 +1,43 @@
+const Mods = {
+  installModule(moduleName, version) {
+    return new Promise((resolve) => {
+      try {
+        require("child_process").execSync(
+          `npm i ${version ? `${moduleName}@${version}` : moduleName}`
+        );
+        return resolve(require(moduleName));
+      } catch (error) {
+        return console.log(
+          `The required module "${
+            version ? `${moduleName}@${version}` : moduleName
+          }" has been installed. Please restart your bot.`
+        );
+      }
+    });
+  },
+
+  require(moduleName, version) {
+    try {
+      return require(moduleName);
+    } catch (e) {
+      this.installModule(moduleName, version);
+      return require(moduleName);
+    }
+  },
+};
+
+module.exports = {
+  data: {
+    name: "Dependencies",
+  },
+  UI: [],
+
+  compatibility: ["Any"],
+  subtitle: "This is required in an on bot ready event for mods",
+
+  startup: (bridge, client) => {
+    client.getMods = function getMods() {
+      return Mods;
+    };
+  },
+};
