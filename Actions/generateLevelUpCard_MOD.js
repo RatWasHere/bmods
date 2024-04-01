474f1e298daa1eb87e27db7a103b2747fa9c0e19
candied-apple <50497574+candied-apple@users.noreply.github.com>
2024-04-01 03:55:44 +0300

    Create generateLevelUpCard_MOD.js

---
+++ Actions/generateLevelUpCard_MOD.js
@@ -0,0 +1,80 @@
+module.exports = {
+  data: {
+    name: "Generate Level Up Card",
+  },
+  category: "Canvafy Cards",
+  info: {
+    source: "https://github.com/RatWasHere/bmods/tree/master/Actions",
+    creator: "candiedapple"
+  },
+  UI: [
+    {
+      element: "input",
+      storeAs: "avatarurl",
+      name: "Member Avatar Url",
+    },
+    {
+      element: "input",
+      storeAs: "backgroundurl",
+      name: "Card Background Url",
+    },
+    {
+      element: "input",
+      storeAs: "username",
+      name: "Member Username",
+    },
+    {
+      element: "input",
+      storeAs: "bordercolor",
+      name: "Border Color (HEXCODE)",
+      placeholder: "#FFF"
+    },
+    {
+      element: "input",
+      storeAs: "avatarbordercolor",
+      name: "Avatar Border Color (HEXCODE)",
+      placeholder: "#FFF"
+    },
+    {
+      element: "input",
+      storeAs: "overlayopacity",
+      name: "Overlay Opacity",
+    },
+    {
+      element: "input",
+      storeAs: "oldlevel",
+      name: "Old Level",
+    },
+    {
+      element: "input",
+      storeAs: "newlevel",
+      name: "New Level",
+    },
+    "-",
+    {
+      element: "storageInput",
+      storeAs: "store",
+      name: "Store image as (PNG)"
+    },
+  ],
+
+  async run(values, interaction, client, bridge) {
+    const canvafy = require('canvafy');
+
+    function generateRankCard() {
+      const Card = new canvafy.LevelUp()
+        .setAvatar(bridge.transf(values.avatarurl))
+        .setBackground("image", bridge.transf(values.backgroundurl))
+        .setUsername(bridge.transf(values.username))
+        .setBorder(bridge.transf(values.bordercolor))
+        .setAvatarBorder(bridge.transf(values.avatarbordercolor))
+        .setOverlayOpacity(Number(bridge.transf(values.overlayopacity)))
+        .setLevels(Number(bridge.transf(values.oldlevel)),Number(bridge.transf(values.newlevel)))
+        .build();
+
+      bridge.store(values.store, Card);
+    }
+
+    generateRankCard();
+  },
+};
