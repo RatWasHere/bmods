094583b5336b37c900fae29c1aa28b0702ca16a1
candied-apple <50497574+candied-apple@users.noreply.github.com>
2024-04-03 00:38:21 +0300

    Create getListElementPosition_MOD.js

---
+++ Actions/getListElementPosition_MOD.js
@@ -0,0 +1,50 @@
+module.exports = {
+    data: {
+        name: "Get List Element Position",
+    },
+    category: "Lists",
+    info: {
+        source: "https://github.com/RatWasHere/bmods/tree/master/Actions",
+        creator: "candiedapple"
+    },
+    UI: [
+        {
+            element: "variableInsertion",
+            storeAs: "list",
+            name: "List",
+        },
+        {
+            element: "input",
+            storeAs: "elementname",
+            name: "Element Name",
+        },
+        "-",
+        {
+            element: "storageInput",
+            storeAs: "store",
+            name: "Store Element Position As",
+        },
+        "-",
+        {
+            element: "text",
+            text: "Note : Positions start at 0",
+        }
+    ],
+
+    async run(values, interaction, client, bridge) {
+
+        const list = bridge.get(values.list)
+        const elementName = bridge.transf(values.elementname)
+
+        const elementPosition = list.indexOf(elementName);
+
+        if (elementPosition !== -1) {
+            console.log(`The position of "${elementName}" in the list is: ${elementPosition}`);
+        } else {
+            console.log(`The element "${elementName}" is not found in the list.`);
+        }
+
+        bridge.store(values.store, elementPosition);
+
+    },
+};
