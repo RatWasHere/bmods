9d1defde21e3ce0afc8e192d8d932f078ec472e0
Nitiqt <158122668+Nitiqt@users.noreply.github.com>
2024-06-27 18:08:11 +0200

    Create getScheduledEvent.js

---
+++ Actions/getScheduledEvent.js
@@ -0,0 +1,36 @@
+module.exports = {
+  data: {
+    name: "Get Scheduled Event",
+  },
+  category: "Scheduled Events",
+  UI: [
+    {
+      element: "guild",
+      storeAs: "guild",
+      name: "Guild"
+    },
+    "-",
+    {
+      element: "input",
+      storeAs: "eventID",
+      name: "Event ID"
+    },
+    "-",
+    {
+      element: "store",
+      name: "Store As",
+      storeAs: "store"
+    }
+  ],
+
+  subtitle: (values, constants, thisAction) => {
+    return `Scheduled Event ${values.eventID} of ${constants.guild(values.guild)} - Store As: ${constants.variable(values.store)}`;
+  },
+
+  async run(values, message, client, bridge) {
+    let eventID = bridge.transf(values.eventID);
+    let guild = await bridge.getGuild(values.guild);
+    let event = await guild.getScheduledEvent(eventID);
+    bridge.store(values.store, event);
+  },
+};
