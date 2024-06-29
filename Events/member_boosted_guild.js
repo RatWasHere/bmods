8f520b087e82eb3f03b68c5face3fd6f82f12ac6
Nitiqt <158122668+Nitiqt@users.noreply.github.com>
2024-06-29 23:52:58 +0200

    Create member_boosted_guild.js

---
+++ Events/member_boosted_guild.js
@@ -0,0 +1,19 @@
+const { Client } = require("oceanic.js");
+
+module.exports = {
+  name: "Member Boosted Server",
+  nameSchemes: ["Store Member As", "Store Server As"],
+  
+  /**
+   * @param {Client} client
+   * @param {*} data
+   * @param {*} run
+   */
+  initialize(client, data, run) {
+    client.on('guildMemberUpdate', async (member, oldMember) => {
+      if (!oldMember.premiumSince && member.premiumSince) {
+        run([member, member.guild], member);
+      }
+    });
+  }
+};
