9b6f5c4a38553207d5b2a7effb2b198d4f814ae5
Nitiqt <158122668+Nitiqt@users.noreply.github.com>
2024-07-01 00:25:36 +0200

    Create getScheduledEvent_MOD.js

---
+++ Actions/getScheduledEvent_MOD.js
@@ -0,0 +1,40 @@
+module.exports = {
+  data: {
+    name: "Get Scheduled Event",
+  },
+  category: "Scheduled Events",
+  info: {
+    source: "https://github.com/RatWasHere/bmods/tree/master/Actions",
+    creator: "nitiqt"
+  },
+  UI: [
+    {
+      element: "guild",
+      storeAs: "guild",
+      name: "Guild"
+    },
+    "-",
+    {
+      element: "input",
+      storeAs: "eventID",
+      name: "Event ID"
+    },
+    "-",
+    {
+      element: "store",
+      name: "Store As",
+      storeAs: "store"
+    }
+  ],
+
+  subtitle: (values, constants, thisAction) => {
+    return `Scheduled Event ${values.eventID} of ${constants.guild(values.guild)} - Store As: ${constants.variable(values.store)}`;
+  },
+
+  async run(values, message, client, bridge) {
+    let eventID = bridge.transf(values.eventID);
+    let guild = await bridge.getGuild(values.guild);
+    let event = await guild.getScheduledEvent(eventID);
+    bridge.store(values.store, event);
+  },
+};
