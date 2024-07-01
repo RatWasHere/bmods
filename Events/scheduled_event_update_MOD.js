64c93b5c5dcdd7cdc0116b5062b2649c1948fccb
Nitiqt <158122668+Nitiqt@users.noreply.github.com>
2024-07-02 00:05:37 +0200

    Update scheduled_event_update_MOD.js

--- Events/scheduled_event_update_MOD.js
+++ Events/scheduled_event_update_MOD.js
@@ -2,7 +2,7 @@ module.exports = {
   name: "Scheduled Event Update",
   nameSchemes: ["Store Scheduled Before Update As", "Store Scheduled After Update As"],
   info: {
-    source: "https://github.com/RatWasHere/bmods/tree/master/Actions",
+    source: "https://github.com/RatWasHere/bmods/tree/master/Events",
     creator: "nitiqt"
   },
   initialize(client, data, run) {
