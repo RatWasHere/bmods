5be208a7930dd1ce3eb06f7fc1bfa3b5edee5f7f
Nitiqt <158122668+Nitiqt@users.noreply.github.com>
2024-07-02 00:06:43 +0200

    Update scheduled_event_user_remove_MOD.js

--- Events/scheduled_event_user_remove_MOD.js
+++ Events/scheduled_event_user_remove_MOD.js
@@ -2,7 +2,7 @@ module.exports = {
   name: "Scheduled Event User Remove",
   nameSchemes: ["Store Scheduled Event ID As", "Store User As"],
   info: {
-    source: "https://github.com/RatWasHere/bmods/tree/master/Actions",
+    source: "https://github.com/RatWasHere/bmods/tree/master/Events",
     creator: "nitiqt"
   },
   initialize(client, data, run) {
