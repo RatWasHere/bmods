a1c1f018212efeaf355ebd692767189a12207d3a
Nitiqt <158122668+Nitiqt@users.noreply.github.com>
2024-07-01 04:41:46 +0200

    Create deleteScheduledEvent_MOD.js

---
+++ Actions/deleteScheduledEvent_MOD.js
@@ -0,0 +1,25 @@
+module.exports = {
+  data: {
+    name: "Delete Scheduled Event",
+  },
+  category: "Scheduled Events",
+  info: {
+    source: "https://github.com/RatWasHere/bmods/tree/master/Actions",
+    creator: "nitiqt"
+  },
+  UI: [
+    {
+      element: "variable",
+      storeAs: "eventVariable",
+      name: "Event Variable",
+    },
+  ],
+  subtitle: (values, constants, thisAction) => {
+    return `Delete Scheduled Event - Event: ${constants.variable(values.eventVariable)}`;
+  },
+
+  async run(values, message, client, bridge) {
+    let event = bridge.variables[values.eventVariable.value];
+    await event.deleteScheduledEvent();
+  },
+};
