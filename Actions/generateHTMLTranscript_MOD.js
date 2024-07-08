6db4027a9385268a2ac43f07b3202e8a0f01ac6d
TheMonDon <11539895+TheMonDon@users.noreply.github.com>
2024-07-08 09:34:48 -0500

    Generate HTML Transcripts (#59)
    * Generate HTML Transcripts

---
+++ Actions/generateHTMLTranscript_MOD.js
@@ -0,0 +1,66 @@
+module.exports = {
+  data: {
+    name: "Generate HTML Transcript",
+  },
+  category: "WebAPIs",
+  info: {
+    source: "https://github.com/RatWasHere/bmods/tree/master/Actions",
+    creator: "TheMonDon",
+  },
+  UI: [
+    {
+      element: "channel",
+      storeAs: "channel",
+      excludeUsers: true,
+    },
+    "-",
+    {
+      element: "dropdown",
+      name: "Save Images",
+      storeAs: "saveImages",
+      choices: [
+        {
+          name: "False",
+        },
+        {
+          name: "True",
+        },
+      ],
+    },
+    {
+      element: "dropdown",
+      name: "Show Footer",
+      storeAs: "showFooter",
+      choices: [
+        {
+          name: "True",
+        },
+        {
+          name: "False",
+        },
+      ],
+    },
+    "-",
+    {
+      element: "storageInput",
+      storeAs: "store",
+    },
+  ],
+
+  async run(values, interaction, client, bridge) {
+    const discordTranscripts = require("oceanic-transcripts");
+    const saveImages = bridge.transf(values.saveImages).toLowerCase();
+    const poweredBy = bridge.transf(values.showFooter).toLowerCase();
+    const channel = await bridge.getChannel(values.channel);
+
+    const attachment = await discordTranscripts.createTranscript(channel, {
+      returnType: "buffer",
+      saveImages,
+      poweredBy,
+    });
+
+    await Promise.all([attachment]).then((results) => {
+      bridge.store(values.store, results[0]);
+    });
+  },
+};
