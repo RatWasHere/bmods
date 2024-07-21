0091406b54f578d9e0c5db711fe84220eced6358
Nitiqt <158122668+Nitiqt@users.noreply.github.com>
2024-07-21 17:11:40 +0200

    Create deleteFolder_MOD.js

---
+++ Actions/deleteFolder_MOD.js
@@ -0,0 +1,26 @@
+module.exports = {
+  data: {
+    name: "Delete Folder",
+  },
+  category: "Files",
+  info: {
+    source: "https://github.com/RatWasHere/bmods/tree/master/Actions",
+    creator: "nitiqt"
+  },
+  UI: [
+    {
+      element: "input",
+      name: "Path",
+      storeAs: "path"
+    }
+  ],
+  subtitle: (data) => {
+    return `Path: ${data.path}`;
+  },
+  compatibility: ["Any"],
+  run(values, message, client, bridge) {
+    let fs = bridge.fs;
+
+    fs.rmSync(bridge.file(values.path), { recursive: true, force: true });
+  },
+};
