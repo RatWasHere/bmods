aec1c44ca008f15ee8a0161f47bfe4ef9130d326
Nitiqt <158122668+Nitiqt@users.noreply.github.com>
2024-07-21 17:12:34 +0200

    Create checkIfFolderExists_MOD.js

---
+++ Actions/checkIfFolderExists_MOD.js
@@ -0,0 +1,44 @@
+module.exports = {
+  data: {
+    name: "Check If Folder Exists",
+  },
+  category: "Files",
+  info: {
+    source: "https://github.com/RatWasHere/bmods/tree/master/Actions",
+    creator: "nitiqt"
+  },
+  UI: [
+    {
+      element: "input",
+      name: "Path",
+      storeAs: "path"
+    },
+    "-",
+    {
+      element: "condition",
+      storeAs: "true",
+      storeActionsAs: "trueActions",
+      name: "If True"
+    },
+    "-",
+    {
+      element: "condition",
+      storeAs: "false",
+      storeActionsAs: "falseActions",
+      name: "If False"
+    }
+  ],
+  subtitle: (data) => {
+    return `Path: ${data.path}`;
+  },
+  compatibility: ["Any"],
+  async run(values, message, client, bridge) {
+    let fs = bridge.fs;
+
+    if (fs.existsSync(bridge.file(values.path)) && fs.statSync(bridge.file(values.path)).isDirectory()) {
+      await bridge.call(values.true, values.trueActions);
+    } else {
+      await bridge.call(values.false, values.falseActions);
+    }
+  },
+};
