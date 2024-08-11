76b1b5f6f2031e057d327c6fe8c7fc87ef3e4e8f
LIK (DANIL) <61782941+likDanil@users.noreply.github.com>
2024-08-11 15:31:51 +0500

    calculate_MOD.js

---
+++ Actions/calculate_MOD.js
@@ -0,0 +1,43 @@
+module.exports = {
+  data: {
+    name: "Calculate PRO",
+  },
+  category: "Numbers",
+  UI: [
+    {
+      element: "input",
+      name: "Operation",
+      storeAs: "operation"
+    },
+    "-",
+    {
+      element: "storageInput",
+      name: "Store Result As",
+      storeAs: "store"
+    },
+    "-",
+    {
+      element: "case",
+      name: "If there is an error",
+      storeAs: "noResults",
+      storeActionsAs: "noResultsActions",
+    }
+  ],
+  subtitle: (data, constants) => {
+    return `${data.operation} - Store Result As ${constants.variable(data.store)}`
+  },
+  compatibility: ["Any"],
+
+  async run(values, message, client, bridge) {
+
+    const { evaluate } = require('mathjs');
+
+    try {
+    let result = 0;
+    result = evaluate(bridge.transf(values.operation));
+    
+    bridge.store(values.store, result)
+  }
+  catch (error) {bridge.call(values.noResults, values.noResultsActions);}
+},
+};
