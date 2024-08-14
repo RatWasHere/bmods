17c6fd90a40aedf8f54e6ae503776a621f048099
Acedia <110048632+sinfuls-acedia@users.noreply.github.com>
2024-08-14 21:36:18 +0800

    Add files via upload

---
+++ Actions/multiRegexCompare.js
@@ -0,0 +1,89 @@
+module.exports = {
+    data: {
+        name: "Multiple Regex Comparisons",
+    },
+    category: "Control",
+
+    info:{
+        source: "https://github.com/RatWasHere/bmods/blob/master/Actions",
+        creator: "Acedia",
+    },
+
+    UI: [
+        {
+            element: "input",
+            storeAs: "input",
+            name: "Value To Compare"
+        },
+        "-",
+        {
+            element: "menu",
+            storeAs: "cases",
+            name: "Comparisons",
+            types: {
+                comparison: "Comparison"
+            },
+            max: 1000,
+            UItypes:{
+                comparison:{
+                    data:{},
+                    name: "Regex",
+                    UI: [
+                        {
+                            element: "input",
+                            storeAs: "value",
+                            name: "Regex Term"
+                        },
+                        "-",
+                        {
+                            element: "condition",
+                            storeAs: "true",
+                            storeActionsAs: "trueActions",
+                            name: "If True"
+                        },
+                        "-",
+                        {
+                            element: "condition",
+                            storeAs: "false",
+                            storeActionsAs: "falseActions",
+                            name: "If False"
+                        }
+                    ]
+                }
+            }
+        }
+    ],
+    
+    subtitle: (data) => {
+        return `Compare ${data.input} To ${data.cases.length} Regex Terms`
+    },
+    
+    compatibility: ["Any"],
+
+    async run(values, message, client, bridge) {
+
+        let firstValue = bridge.transf(values.input);
+        let matchesCriteria = false;
+        let comparison = null;
+    
+        for (let c in values.cases) {
+            comparison = values.cases[c].data;
+            let secondValue = bridge.transf(comparison.value);
+    
+            matchesCriteria = Boolean(
+                firstValue.match(new RegExp("^" + secondValue + "$", "i"))
+            );
+    
+            if (matchesCriteria) {
+                break;
+            }
+        }
+    
+        if (matchesCriteria) {
+            await bridge.call(comparison.true, comparison.trueActions);
+        } else {
+            await bridge.call(comparison.false, comparison.falseActions);
+        }
+    }
+    
+}
