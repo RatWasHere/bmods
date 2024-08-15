361bfc40342caf1ec80678864085fe08bbf371e4
Acedia <110048632+slothyace@users.noreply.github.com>
2024-08-15 20:21:04 +0800

    Add files via upload

---
+++ Actions/loadMultipleGlobalData_MOD.js
@@ -0,0 +1,66 @@
+module.exports = {
+    data: { name: "Get multiple global data" },
+    category: "Global Data",
+    UI: [
+        {
+            element: "input",
+            storeAs: "defaultval",
+            name: "Default value"
+        },
+        {
+            element: "menu",
+            storeAs: "retrievelist",
+            name: "Global data",
+            types: {
+                data: "datas"
+            },
+            max: 1000,
+            UItypes: {
+                data: {
+                    data: {},
+                    name: "Global data name",
+                    UI: [
+                        {
+                            element: "input",
+                            storeAs: "dataname",
+                            name: "Global data name"
+                        },
+                        {
+                            element: "store",
+                            storeAs: "store"
+                        }
+                    ]
+                }
+            }
+        }
+    ],
+    subtitle: (data) => {
+        return `Store ${data.retrievelist.length} global data(s).`;
+    },
+    compatibility: ["Any"],
+
+    async run(values, message, client, bridge) {
+        let storedData = bridge.data.IO.get();
+        let defaultVal = values.defaultval ? bridge.transf(values.defaultval) : '';
+
+        for (let item of values.retrievelist) {
+            let listData = defaultVal;
+
+            const dataName = item.data.dataname;
+            const storeLocation = item.data.store;
+
+            try {
+                const transformedDataName = bridge.transf(dataName);
+
+                if (storedData.lists && storedData.lists[transformedDataName]) {
+                    listData = storedData.lists[transformedDataName];
+                }
+            } catch (error) {
+                storedData.lists = {};
+                bridge.data.IO.write(storedData);
+            }
+
+            bridge.store(storeLocation, listData);
+        }
+    }
+}
