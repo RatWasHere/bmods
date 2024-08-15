24e04aec51d04e539d9f13207e0ffed7e3b450e4
Acedia <110048632+slothyace@users.noreply.github.com>
2024-08-16 00:02:37 +0800

    Update loadMultipleGlobalData_MOD.js

--- Actions/loadMultipleGlobalData_MOD.js
+++ Actions/loadMultipleGlobalData_MOD.js
@@ -11,6 +11,7 @@ module.exports = {
             storeAs: "defaultval",
             name: "Default value"
         },
+        "-",
         {
             element: "menu",
             storeAs: "retrievelist",
