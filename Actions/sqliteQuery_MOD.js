5ae5afcbf6d525abed71846a4d517df6a3ebf909
candied apple <alperenger@gmail.com>
2024-08-22 17:14:35 +0300

    added donate link and updated mysql mod (#85)

--- Actions/sqliteQuery_MOD.js
+++ Actions/sqliteQuery_MOD.js
@@ -6,6 +6,7 @@ module.exports = {
   info: {
     source: "https://github.com/RatWasHere/bmods/tree/master/Actions",
     creator: "candiedapple",
+    donate: "https://buymeacoffee.com/candiedapple",
   },
   UI: [
     {
