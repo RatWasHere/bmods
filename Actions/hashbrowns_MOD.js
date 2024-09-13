61e0a9a96131c87e5ef50d11ab0992023bebe983
Acedia <110048632+slothyace@users.noreply.github.com>
2024-09-13 23:13:56 +0800

    Added "Hash" action
    yep, i named the file hashbrowns, sue me

---
+++ Actions/hashbrowns_MOD.js
@@ -0,0 +1,53 @@
+const crypto = require('node:crypto');
+acpthashes = crypto.getHashes().toString();
+
+module.exports = {
+  data: {
+    name: "Hash"
+  },
+  info: {
+    source: "https://github.com/slothyace/BCS/blob/main/Mods",
+    creator: "Acedia",
+    donate: "https://ko-fi.com/slothyacedia"
+  },
+  category: "Encrypt",
+  UI: [
+    {
+      element: "largeInput",
+      storeAs: "toHash",
+      name: "Text",
+    },
+    {
+      element: "input",
+      storeAs: "algorithm",
+      name: "Hashing Algorithm",
+      placeholder: "If left empty, SHA256 will be used."
+    },
+    "-",
+    {
+      element: "store",
+      storeAs: "store",
+      name: "Store Hashed String As"
+    },
+    "-",
+    {
+      element: "text",
+      text: `Supported algorithms: ${acpthashes}`,
+    }
+  ],
+  
+  subtitle: (values) => {
+    let algorythm = values.algorithm || `SHA256`;
+    return `Hash text with ${algorythm} algorithm`
+  },
+
+  compatibility: ["Any"],
+
+  async run (values, message, client, bridge) {
+    let oriText = bridge.transf(values.toHash);
+    let hashAlgorithm = bridge.transf(values.algorithm) || `SHA256`;
+    
+    hashedTxt = crypto.createHash(hashAlgorithm).update(oriText).digest('hex');
+    bridge.store(values.store, hashedTxt);
+  }
+}
