627ff586fc66877de36f0a7b1068f17390a65559
DevvyyXYZ <132696257+devvyyxyz@users.noreply.github.com>
2024-11-20 16:06:19 +0000

    Member timeout add/remove event

---
+++ Events/member_timeout_add.js
@@ -0,0 +1,19 @@
+const { Client } = require("oceanic.js");
+
+module.exports = {
+  name: "Member Timeout Add",
+  nameSchemes: ["Store Member As", "Store Server As"],
+  
+  /**
+   * @param {Client} client
+   * @param {*} data
+   * @param {*} run
+   */
+  initialize(client, data, run) {
+    client.on('guildMemberUpdate', async (member, oldMember) => {
+      if (!oldMember.communicationDisabledUntil && member.communicationDisabledUntil) {
+        run([member, member.guild], member);
+      }
+    });
+  }
+};
