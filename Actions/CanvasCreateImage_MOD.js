105a65887923a40cf6e4d01c3798cdd88c176ffa
LIK (DANIL) <61782941+likDanil@users.noreply.github.com>
2025-03-29 00:14:54 +0500

    CanvasCreateImage_MOD.js (Upd) (#118)
    Added resizing

--- Actions/CanvasCreateImage_MOD.js
+++ Actions/CanvasCreateImage_MOD.js
@@ -6,7 +6,7 @@ module.exports = {
   category: "Canvas Images",
   info: {
     source: "https://github.com/RatWasHere/bmods/tree/master/Actions",
-    creator: "TheMonDon",
+    creator: "TheMonDon , lik_rus",
   },
   UI: [
     {
@@ -16,22 +16,54 @@ module.exports = {
     },
     "-",
     {
+      element: "inputGroup",
+      storeAs: ["Width", "Height"],
+      nameSchemes: ["Width (px or %)", "Height (px or %)"]
+    },
+    "-",
+    {
       element: "storageInput",
       storeAs: "store",
-    },
+    }
   ],
 
   async run(values, interaction, client, bridge) {
     const Canvas = await client.getMods().require("canvas");
     try {
       const image = await Canvas.loadImage(bridge.transf(values.url));
-      const canvas = Canvas.createCanvas(image.width, image.height);
+      var scalex = bridge.transf(values.Width)
+      var scaley = bridge.transf(values.Height)
+      if (scalex == '') { scalex = "100%" }
+      if (scaley == '') { scaley = "100%" }
+      let imagew = image.width
+      let imageh = image.height
+      let scalew = 1
+      let scaleh = 1
+      scale(scalex, scaley)
+      const canvas = Canvas.createCanvas(imagew, imageh);
       const ctx = canvas.getContext("2d");
-      ctx.drawImage(image, 0, 0, image.width, image.height);
+      ctx.drawImage(image, 0, 0, imagew, imageh);
       const result = canvas.toBuffer();
       bridge.store(values.store, result);
+
+      function scale(w, h) {
+        if (w.endsWith('%')) {
+          const percent = w.replace('%', '')
+          scalew = parseInt(percent) / 100
+        } else {
+          scalew = parseInt(w) / imagew
+        }
+        if (h.endsWith('%')) {
+          const percent = h.replace('%', '')
+          scaleh = parseInt(percent) / 100
+        } else {
+          scaleh = parseInt(h) / imageh
+        }
+        imagew *= scalew
+        imageh *= scaleh
+      }
     } catch (error) {
-      console.error("Error:", error);
-    }
+        console.error("Error:", error);
+      }
   },
 };
