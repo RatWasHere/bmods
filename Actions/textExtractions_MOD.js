b7a91835518c62de2814f6f492e58dcaeca2bae5
Acedia <110048632+slothyace@users.noreply.github.com>
2025-06-24 10:01:56 +0800

    update text extraction mod
    Has a easier to understand option now and updated number to include negative numbers too

--- Actions/textExtractions_MOD.js
+++ Actions/textExtractions_MOD.js
@@ -1,9 +1,9 @@
-modVersion = "v1.1.0"
+modVersion = "v1.2.4"
 module.exports = {
   data: {
     name: "Extract From Text"
   },
-  aliases: ["Number Extraction", "Regex Extraction"],
+  aliases: ["Number Extraction", "Regex Extraction", "Text Extractions"],
   modules: [],
   category: "Text",
   info: {
@@ -17,14 +17,23 @@ module.exports = {
       storeAs: "sourceText",
       name: "Source"
     },
+    "-",
     {
       element: "typedDropdown",
       storeAs: "extraction",
       name: "Extract",
       choices: {
-        string: {name: "String", field: true, placeholder: "Regex"},
-        number: {name: "Number", field: false}
+        string: {name: "Regex", field: true, placeholder: "Regex"},
+        number: {name: "Numbers", field: false},
+        text: {name: "Words", field: true, placeholder: "Words That Include"}
       },
+    },
+    {
+      element: "toggle",
+      storeAs: "caseInsensitive",
+      name: "Case Insensitive?",
+      true: "Yes",
+      false: "No",
     },
     "-",
     {
@@ -40,13 +49,24 @@ module.exports = {
 
   subtitle: (values, constants, thisAction) => {
     let type = values.extraction.type
-    let regexExp
-    if (type == "string"){
-      regexExp = values.extraction.value.replace("\\", "\\\\") || ""
-    } else if (type == "number"){
-      regexExp = `(\\d+(?:\\.\\d+)?)`
+    let subtitle
+    switch (type){
+      case "string":{
+        let regexExp = values.extraction.value.replace("\\", "\\\\") || ""
+        subtitle = `Extract Regex(${regexExp})`
+        break
+      }
+
+      case "number":{
+        subtitle = `Extract Numbers`
+        break
+      }
+
+      case "text":{
+        subtitle = `Extract Words That Includes "${values.extraction.value}"`
+      }
     }
-    return `Extract ${thisAction.UI.find((e) => e.element == "typedDropdown").choices[values.extraction.type].name}(${regexExp})`
+    return subtitle
   },
 
   compatibility: ["Any"],
@@ -57,14 +77,30 @@ module.exports = {
 
     let extracts
     switch(extractionType){
-      case "string":
+      case "string":{
         let extractionReg = bridge.transf(values.extraction.value) || ""
-        extracts = [...source.matchAll(new RegExp("^"+extractionReg+"$", "g"))].map((match) => match[0])
+        let regexExpression = new RegExp(extractionReg, "g" + (values.caseInsensitive? "i":""))
+        extracts = [...source.matchAll(regexExpression)].map((match) => match[0])
         break
+      }
       
-      case "number":
-        extracts = [...source.matchAll(/(\d+(?:\.\d+)?)/g)].map((match) => match[0])
+      case "number":{
+        extracts = (source.match(/-?\d+(?:\.\d+)?/g) || [])
         break
+      }
+
+      case "text":{
+        let lookFor = bridge.transf(values.extraction.value) || ""
+        let words = source.split(" ")
+        if (values.caseInsensitive){
+          extracts = words.filter(word => word.toLowerCase().includes(lookFor.toLowerCase()))
+        } else {
+          extracts = words.filter(word => word.includes(lookFor))
+        }
+
+        extracts = (extracts || []).map(extractedWord => extractedWord.replace(/[!"#$%&'()*+,./:;<=>?@[\\\]^_`{|}~‘’“”]/g, ""))
+        break
+      }
     }
     let results = (extracts && extracts.length > 0) ? extracts : []
     bridge.store(values.extractedItem, results)
