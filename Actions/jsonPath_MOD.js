abcfb1e916e484e2638476d1c795018f673423cc
Fusionist-0 <116846620+Fusionist-0@users.noreply.github.com>
2025-11-22 22:38:20 -0400

    Update jsonPath_MOD.js

--- Actions/jsonPath_MOD.js
+++ Actions/jsonPath_MOD.js
@@ -12,13 +12,13 @@ module.exports = {
   UI: [
     {
       element: "variableInsertion",
-      name: "Json (Must be json object)",
+      name: "JSON (Must be JSON object)",
       storeAs: "json",
     },
     "-",
     {
       element: "input",
-      name: `Json Path
+      name: `JSON Path
             <div class="hoverablez" onclick="require('electron').shell.openExternal('https://goessner.net/articles/JsonPath/')" style="width: 20%; margin: left; padding-bottom: 12px; margin-bottom: -12px; border-radius: 8px;"><text style="margin-left:8px; padding: 3px;">Click for Guide</text></div>`,
       storeAs: "path",
       placeholder: "$.store.book[*].author",
