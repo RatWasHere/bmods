b3f950ef27dd7619f40feb78c4bfff510609ebde
Fusionist-0 <116846620+Fusionist-0@users.noreply.github.com>
2026-02-12 14:30:18 -0400

    Add files via upload

---
+++ Actions/publishAnnouncementMessage_MOD.js
@@ -0,0 +1,38 @@
+module.exports = {
+  category: "Messages",
+  data: {
+    name: "Publish Announcement Message"
+  },
+  UI: [
+    {
+      element: "message",
+      storeAs: "message",
+      name: "Message"
+    },
+    "-",
+    {
+      element: "storage",
+      storeAs: "store",
+      name: "Store Published Message As",
+      optional: true
+    }
+  ],
+
+  compatibility: ["Any"],
+  subtitle: (values, constants) => {
+    return `${constants.message(values.message)}`
+  },
+
+  async run(values, msg, client, bridge) {
+    const message = await bridge.getMessage(values.message);
+    if (!message) return;
+
+    try {
+      const publishedMessage = await message.crosspost();
+      if (values.store && values.store.type != "none") {
+        bridge.store(values.store, publishedMessage ?? message);
+      }
+    } catch {
+    }
+  },
+};
